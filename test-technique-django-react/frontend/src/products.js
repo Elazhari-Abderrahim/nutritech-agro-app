216a617bddb2988cec13de3cdec70676070f52eb
abderrahim <aelazhari0@gmail.com>
2024-11-05 18:25:19 +0100

    initial commit

---
+++ test-technique-django-react/frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "",
+    image: "",
+    description:
+      "",
+    brand: "",
+    category: "",
+    price: "",
+    countInStock: "",
+    rating: "",
+    numReviews: "",
+  },
+  {
+    _id: "2",
+    name: "",
+    image: "",
+    description:
+      "",
+    brand: "",
+    category: "",
+    price: "",
+    countInStock: "",
+    rating: "",
+    numReviews: "",
+  },
+  {
+    _id: "2",
+    name: "",
+    image: "",
+    description:
+      "",
+    brand: "",
+    category: "",
+    price: "",
+    countInStock: "",
+    rating: "",
+    numReviews: "",
+  },
+  {
+    _id: "3",
+    name: "",
+    image: "",
+    description:
+      "",
+    brand: "",
+    category: "",
+    price: "",
+    countInStock: "",
+    rating: "",
+    numReviews: "",
+  },
+  {
+    _id: "5",
+    name: "",
+    image: "",
+    description:
+      "",
+    brand: "",
+    category: "",
+    price: "",
+    countInStock: "",
+    rating: "",
+    numReviews: "",
+  },
+  {
+    _id: "6",
+    name: "",
+    image: "",
+    description:
+      "",
+    brand: "",
+    category: "",
+    price: "",
+    countInStock: "",
+    rating: "",
+    numReviews: "",
+  },
+];
+
+export default products;
